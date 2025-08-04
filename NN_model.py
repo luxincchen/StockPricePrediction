253bb65334ff93d3924931e16e4aa6b2cb7aa2b2
Luxin Chen <rachel@eduvpn-uva-stu-145-3-16-151.vpn.uva.nl>
2025-08-04 18:38:53 +0800

    unfinished bc I ran into a problem

--- NN_model.py
+++ NN_model.py
@@ -1,2 +1,18 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+
+class StockNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Flatten(),             
+            nn.Linear(60, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+            )
+
+    def forward(self, x):
+        return self.model(x)
